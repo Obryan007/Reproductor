cd067b6aaa97f789807617ca9a422874d2b5eda2
ksalazar <asalazar@solucionesun.com>
2024-04-12 19:59:24 -0600

    Grupo 4

---
+++ PlayerBlazorApp/Models/Nodo.cs
@@ -0,0 +1,121 @@
+﻿namespace PlayerBlazorApp.Models
+{
+    public class Video
+    {
+        public string Titulo { get; set; }
+        public string RutaArchivo { get; set; }
+
+        public class SubVideo // Cambia el nombre de la clase interna
+        {
+            public string Titulo { get; set; }
+            public string RutaArchivo { get; set; }
+
+            public SubVideo(string titulo, string rutaArchivo)
+            {
+                Titulo = titulo;
+                RutaArchivo = rutaArchivo;
+            }
+        }
+
+        // Método para actualizar el título del video
+        public void ActualizarTitulo(string nuevoTitulo)
+        {
+            Titulo = nuevoTitulo;
+        }
+
+        // Método para actualizar la ruta del archivo del video
+        public void ActualizarRutaArchivo(string nuevaRuta)
+        {
+            RutaArchivo = nuevaRuta;
+        }
+    }
+}
+
+namespace PlayerBlazorApp.Models
+{
+    public class Nodo
+    {
+        public Nodo? LigaAnterior { get; set; }
+        public Video.SubVideo Datos { get; set; } // Cambia el tipo de Datos a Video.SubVideo
+        public Nodo? LigaSiguiente { get; set; }
+
+        // Cambia el constructor para aceptar Video.SubVideo
+        public Nodo(Video.SubVideo datos)
+        {
+            LigaAnterior = null;
+            Datos = datos;
+            LigaSiguiente = null;
+        }
+
+        // Método para obtener el nodo anterior
+        public Nodo? ObtenerNodoAnterior()
+        {
+            return LigaAnterior;
+        }
+
+        // Método para obtener el nodo siguiente
+        public Nodo? ObtenerNodoSiguiente()
+        {
+            return LigaSiguiente;
+        }
+
+        // Método para verificar si el nodo tiene un nodo anterior
+        public bool TieneNodoAnterior()
+        {
+            return LigaAnterior != null;
+        }
+
+        // Método para verificar si el nodo tiene un nodo siguiente
+        public bool TieneNodoSiguiente()
+        {
+            return LigaSiguiente != null;
+        }
+
+        // Método para insertar un nuevo nodo después del nodo actual
+        public void InsertarNodoDespues(Nodo nuevoNodo)
+        {
+            if (this.LigaSiguiente != null)
+            {
+                nuevoNodo.LigaAnterior = this;
+                nuevoNodo.LigaSiguiente = this.LigaSiguiente;
+                this.LigaSiguiente.LigaAnterior = nuevoNodo;
+                this.LigaSiguiente = nuevoNodo;
+            }
+            else
+            {
+                this.LigaSiguiente = nuevoNodo;
+                nuevoNodo.LigaAnterior = this;
+            }
+        }
+
+        // Método para insertar un nuevo nodo antes del nodo actual
+        public void InsertarNodoAntes(Nodo nuevoNodo)
+        {
+            if (this.LigaAnterior != null)
+            {
+                nuevoNodo.LigaSiguiente = this;
+                nuevoNodo.LigaAnterior = this.LigaAnterior;
+                this.LigaAnterior.LigaSiguiente = nuevoNodo;
+                this.LigaAnterior = nuevoNodo;
+            }
+            else
+            {
+                this.LigaAnterior = nuevoNodo;
+                nuevoNodo.LigaSiguiente = this;
+            }
+        }
+
+        // Método para eliminar este nodo de la lista
+        public void EliminarNodo()
+        {
+            if (this.LigaAnterior != null)
+            {
+                this.LigaAnterior.LigaSiguiente = this.LigaSiguiente;
+            }
+            if (this.LigaSiguiente != null)
+            {
+                this.LigaSiguiente.LigaAnterior = this.LigaAnterior;
+            }
+        }
+    }
+}
---
+++ PlayerBlazorApp/Services/DoubleLinkedList.cs
@@ -0,0 +1,171 @@
+﻿using PlayerBlazorApp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PlayerBlazorApp.Services
+{
+    public class DoubleLinkedList
+    {
+        public Nodo? PrimerNodo { get; set; }
+        public Nodo? UltimoNodo { get; set; }
+        public Nodo? NodoActual { get; set; }
+
+        public DoubleLinkedList()
+        {
+            PrimerNodo = null;
+            UltimoNodo = null;
+            NodoActual = null;
+        }
+
+        public bool IsEmpty => PrimerNodo == null;
+
+        public string AgregarVideoAlInicio(Video.SubVideo video)
+        {
+            Nodo nuevoNodo = new Nodo(video);
+
+            if (IsEmpty)
+            {
+                PrimerNodo = nuevoNodo;
+                UltimoNodo = nuevoNodo;
+            }
+            else
+            {
+                nuevoNodo.LigaSiguiente = PrimerNodo;
+                PrimerNodo.LigaAnterior = nuevoNodo;
+                PrimerNodo = nuevoNodo;
+            }
+
+            NodoActual = nuevoNodo;
+
+            return "Video agregado al inicio...";
+        }
+        public List<string> ObtenerTitulos()
+        {
+            var titulos = new List<string>();
+            var actual = PrimerNodo;
+
+            while (actual != null)
+            {
+                titulos.Add(actual.Datos.Titulo); // Cambio aquí
+                actual = actual.LigaSiguiente;
+            }
+
+            return titulos;
+        }
+        public string AgregarVideoAlFinal(Video.SubVideo video)
+        {
+            Nodo nuevoNodo = new Nodo(video);
+
+            if (IsEmpty)
+            {
+                PrimerNodo = nuevoNodo;
+                UltimoNodo = nuevoNodo;
+            }
+            else
+            {
+                UltimoNodo.LigaSiguiente = nuevoNodo;
+                nuevoNodo.LigaAnterior = UltimoNodo;
+                UltimoNodo = nuevoNodo;
+            }
+
+            NodoActual = nuevoNodo;
+
+            return "Video agregado al final...";
+        }
+
+        public void EliminarVideoAlInicio()
+        {
+            if (IsEmpty)
+            {
+                Console.WriteLine("La lista está vacía.");
+                return;
+            }
+
+            if (PrimerNodo == UltimoNodo)
+            {
+                PrimerNodo = null;
+                UltimoNodo = null;
+            }
+            else
+            {
+                PrimerNodo = PrimerNodo.LigaSiguiente;
+                PrimerNodo.LigaAnterior = null;
+            }
+        }
+
+        public void EliminarVideoAlFinal()
+        {
+            if (IsEmpty)
+            {
+                Console.WriteLine("La lista está vacía.");
+                return;
+            }
+
+            if (PrimerNodo == UltimoNodo)
+            {
+                PrimerNodo = null;
+                UltimoNodo = null;
+            }
+            else
+            {
+                UltimoNodo = UltimoNodo.LigaAnterior;
+                UltimoNodo.LigaSiguiente = null;
+            }
+        }
+
+        public void EliminarPosicionSeleccionada()
+        {
+            if (IsEmpty || NodoActual == null)
+            {
+                Console.WriteLine("La lista está vacía o no hay un nodo actual seleccionado.");
+                return;
+            }
+
+            if (NodoActual == PrimerNodo)
+            {
+                EliminarVideoAlInicio();
+            }
+            else if (NodoActual == UltimoNodo)
+            {
+                EliminarVideoAlFinal();
+            }
+            else
+            {
+                NodoActual.LigaAnterior.LigaSiguiente = NodoActual.LigaSiguiente;
+                NodoActual.LigaSiguiente.LigaAnterior = NodoActual.LigaAnterior;
+                NodoActual = null;
+            }
+        }
+
+        public List<string> ObtenerEnlaces()
+        {
+            var enlaces = new List<string>();
+            var actual = PrimerNodo;
+
+            while (actual != null)
+            {
+                enlaces.Add(actual.Datos.RutaArchivo);
+                actual = actual.LigaSiguiente;
+            }
+
+            return enlaces;
+        }
+
+        public Nodo? ObtenerPosicionNodo(int posicion)
+        {
+            if (posicion < 1)
+                return null;
+
+            int contador = 1;
+            Nodo? actual = PrimerNodo;
+
+            while (actual != null && contador < posicion)
+            {
+                actual = actual.LigaSiguiente;
+                contador++;
+            }
+
+            return actual;
+        }
+    }
+}
